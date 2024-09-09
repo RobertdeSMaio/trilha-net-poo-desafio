1f1fc384b10297f19ecf1d69a13cbcfaef43256e
Robert <robertmaio@outlook.com>
2024-09-09 14:43:00 -0300

    add the todo solved

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+        // Test Iphone
+        Iphone i11 = new Iphone("22992843155", "Iphone11", "123456789012345", 128);
+        Console.WriteLine("Iphone Number: " + i11.Numero);
+        // Test Nokia
+        Nokia n95 = new Nokia("33993847982", "G11", "987654321098765", 64);
+        Console.WriteLine("Nokia Number: " + n95.Numero);
